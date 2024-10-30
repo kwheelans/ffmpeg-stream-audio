a07be75aa31d5f9fd0a3fc99d5ed3609ea34f335
Kevin Wheelans <kevin.wheelans@proton.me>
2024-10-30 15:43:08 -0400

    use tokio::process instead of std::process to better handle potential zombie process

--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,32 @@ mod config;
 mod error;
 mod cli;
 
+use std::process::{ExitCode, Stdio};
 use crate::cli::CliArgs;
 use crate::config::StreamConfig;
 use crate::error::Error;
 use clap::Parser;
-use std::process::{Command, ExitCode, Stdio};
+use tokio::process::Command;
 
 fn main() -> ExitCode {
-    match run() {
+    match tokio::runtime::Builder::new_current_thread().enable_all().build() {
+        Ok(rt) => {
+            match rt.block_on(run()) {
+                Err(error) => {
+                    println!("{}", error);
+                    ExitCode::FAILURE
+                }
+                Ok(_) => ExitCode::SUCCESS,
+            }
+        }
         Err(error) => {
             println!("{}", error);
             ExitCode::FAILURE
         }
-        Ok(_) => ExitCode::SUCCESS,
     }
-
 }
 
-fn run () -> Result<(), Error>{
+async fn run () -> Result<(), Error>{
     let cli = CliArgs::parse();
     let config = StreamConfig::try_from(std::fs::read_to_string(cli.configuration)?.as_str())?;
     println!("{:?}", config);
@@ -27,13 +35,13 @@ fn run () -> Result<(), Error>{
     let mut ffmpeg = Command::new("ffmpeg")
         .stdout(Stdio::piped())
         .args(config.to_vec())
+        .kill_on_drop(true)
         .spawn()?;
-
-
+    
     println!("{:?}", ffmpeg);
     //let stdout = ffmpeg.stdout.take().unwrap();
 
-    _ = ffmpeg.wait();
+    _ = ffmpeg.wait().await?;
     println!("process exited");
     Ok(())
 }
