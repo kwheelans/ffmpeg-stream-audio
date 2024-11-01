f89433955b6b074bef9c0efad3fb1bf07185fd5a
Kevin Wheelans <kevin.wheelans@proton.me>
2024-11-01 17:14:06 -0400

    make configuration more modular

--- src/config.rs
+++ src/config.rs
@@ -1,6 +1,28 @@
-use std::ffi::OsString;
 use crate::error::Error;
 use serde::Deserialize;
+use std::ffi::OsString;
+use std::fmt::Debug;
+
+// General Options
+const FORMAT_OPT: &str = "-f";
+const INPUT_OPT: &str = "-i";
+
+// Audio Options
+const CODEC_OPT: &str = "-codec:a";
+const SAMPLE_RATE_OPT: &str = "-ar";
+const CHANNELS_OPT: &str = "-ac";
+const SAMPLE_FORMAT: &str = "-sample_fmt";
+const CONTENT_TYPE_OPT: &str = "-content_type";
+
+// FLAC Options
+const COMPRESSION_LEVEL_OPT: &str = "-compression_level";
+
+pub trait CommandConfig: Debug {
+    fn to_vec(&self) -> Vec<OsString>;
+}
+
+#[typetag::deserialize(tag = "codec")]
+trait AudioCodec: CommandConfig {}
 
 #[derive(Deserialize, Debug)]
 pub struct StreamConfig {
@@ -12,17 +34,27 @@ pub struct StreamConfig {
 struct StreamInput {
     input: String,
     input_type: String,
+    #[serde(default)]
+    overwrite: bool,
+    sample_rate: Option<u32>,
+    sample_format: Option<String>,
+    codec: Option<String>,
 }
 
 #[derive(Deserialize, Debug)]
 struct StreamOutput {
     output: String,
-    channels: String,
-    sample_rate: String,
-    codec: String,
-    compression_level: String,
+    channels: Option<String>,
+    sample_rate: u32,
+    codec: Box<dyn AudioCodec>,
+
     container: String,
     content_type: Option<String>,
+}
+
+#[derive(Deserialize, Debug)]
+struct Flac {
+    compression_level: Option<u8>,
 }
 
 impl TryFrom<&str> for StreamConfig {
@@ -33,31 +65,83 @@ impl TryFrom<&str> for StreamConfig {
     }
 }
 
-impl StreamConfig {
-    pub fn to_vec(self) -> Vec<OsString> {
-        let mut value = vec![
-            "-f".into(),
-            self.input.input_type.into(),
-            "-i".into(),
-            self.input.input.into(),
-            "-ac".into(),
-            self.output.channels.into(),
-            "-ar".into(),
-            self.output.sample_rate.into(),
-            "-c:a".into(),
-            self.output.codec.into(),
-            "-compression_level".into(),
-            self.output.compression_level.into(),
-        ];
-        if self.output.content_type.is_some() {
-            value.push("-content_type".into());
-            value.push(self.output.content_type.unwrap().into())
+impl CommandConfig for StreamConfig {
+    fn to_vec(&self) -> Vec<OsString> {
+        let mut value = self.input.to_vec();
+        value.extend(self.output.to_vec());
+        value
+    }
+}
+
+impl CommandConfig for StreamInput {
+    fn to_vec(&self) -> Vec<OsString> {
+        let overwrite = match self.overwrite {
+            true => "-y",
+            false => "-n",
+        };
+        let mut input = vec!["-hide_banner".into(), overwrite.into()];
+
+        if let Some(codec) = &self.codec {
+            input.push(CODEC_OPT.into());
+            input.push(codec.into());
+        }
+
+        if let Some(sample_format) = &self.sample_format {
+            input.push(SAMPLE_FORMAT.into());
+            input.push(sample_format.as_str().into());
+        }
+
+        input.push(FORMAT_OPT.into());
+        input.push(self.input_type.as_str().into());
+
+        if let Some(sample_rate) = self.sample_rate {
+            input.push(SAMPLE_RATE_OPT.into());
+            input.push(sample_rate.to_string().into());
+        }
+
+        input.push(INPUT_OPT.into());
+        input.push(self.input.as_str().into());
+
+        input
+    }
+}
+
+impl CommandConfig for StreamOutput {
+    fn to_vec(&self) -> Vec<OsString> {
+        let mut value = vec![SAMPLE_RATE_OPT.into(), self.sample_rate.to_string().into()];
+
+        if let Some(channels) = &self.channels {
+            value.push(CHANNELS_OPT.into());
+            value.push(channels.into());
         }
         
-        value.push("-f".into());
-        value.push(self.output.container.into());
-        value.push(self.output.output.into());
-        
+        value.extend(self.codec.to_vec());
+
+        if let Some(content_type) = &self.content_type {
+            value.push(CONTENT_TYPE_OPT.into());
+            value.push(OsString::from(content_type));
+        }
+
+        value.push(FORMAT_OPT.into());
+        value.push(self.container.as_str().into());
+
+        value.push(self.output.as_str().into());
         value
+    }
+}
+
+#[typetag::deserialize(name = "flac")]
+impl AudioCodec for Flac {}
+
+impl CommandConfig for Flac {
+    fn to_vec(&self) -> Vec<OsString> {
+        let mut codec = vec![CODEC_OPT.into(), "flac".into()];
+
+        if let Some(compression) = self.compression_level {
+            codec.push(COMPRESSION_LEVEL_OPT.into());
+            codec.push(compression.to_string().into())
+        }
+
+        codec
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,29 @@
+mod cli;
 mod config;
 mod error;
-mod cli;
 
-use std::process::{ExitCode, Stdio};
 use crate::cli::CliArgs;
-use crate::config::StreamConfig;
+use crate::config::{CommandConfig, StreamConfig};
 use crate::error::Error;
 use clap::Parser;
+use std::process::{ExitCode, Stdio};
 use tokio::process::Command;
 
 fn main() -> ExitCode {
-    match tokio::runtime::Builder::new_current_thread().enable_all().build() {
-        Ok(rt) => {
-            match rt.block_on(run()) {
-                Err(error) => {
-                    println!("{}", error);
-                    ExitCode::FAILURE
-                }
-                Ok(code) => {
-                    match code {
-                        None => ExitCode::FAILURE,
-                        Some(n) => ExitCode::from(n as u8)
-                    }
-                },
+    match tokio::runtime::Builder::new_current_thread()
+        .enable_all()
+        .build()
+    {
+        Ok(rt) => match rt.block_on(run()) {
+            Err(error) => {
+                println!("{}", error);
+                ExitCode::FAILURE
             }
-        }
+            Ok(code) => match code {
+                None => ExitCode::FAILURE,
+                Some(n) => ExitCode::from(n as u8),
+            },
+        },
         Err(error) => {
             println!("{}", error);
             ExitCode::FAILURE
@@ -32,19 +31,18 @@ fn main() -> ExitCode {
     }
 }
 
-async fn run () -> Result<Option<i32>, Error>{
+async fn run() -> Result<Option<i32>, Error> {
     let cli = CliArgs::parse();
     let config = StreamConfig::try_from(std::fs::read_to_string(cli.configuration)?.as_str())?;
     println!("{:?}", config);
-    
+
     let mut ffmpeg = Command::new("ffmpeg")
         .stdout(Stdio::piped())
         .args(config.to_vec())
         .kill_on_drop(true)
         .spawn()?;
-    
+
     println!("{:?}", ffmpeg);
-    //let stdout = ffmpeg.stdout.take().unwrap();
-    
+
     Ok(ffmpeg.wait().await?.code())
 }
